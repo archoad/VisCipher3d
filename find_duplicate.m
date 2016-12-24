eb806090e18fbd08ff8db98de325d02f71c13c05
MyShell <myshell.dubois@gmail.com>
2016-12-24 11:31:27 +0100

    Finding duplicates

--- find_duplicate.m
+++ find_duplicate.m
@@ -1,10 +1,14 @@
 clear all;
-printf("Data analysis\n");
+
+printf('Data analysis\n');
+
 tic;
 load analyse.dat;
 [D,~,X] = unique(data);
 [n, bin] = histc(X,unique(X));
 multiple = find(n > 1);
 index = find(ismember(bin, multiple));
-toc;printf("Result is:\n");
+toc;
+
+printf('Result is:\n');
 index
